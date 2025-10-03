40113f69d5c5fee501ad9a4517672483e75bcd18
jy37414 <134957895+jy37414@users.noreply.github.com>
2025-10-03 19:41:23 +0900

    refactor #008 : ServiceCache 수정, gitignore에 dev파일 추가

--- src/main/java/com/cu2mber/gatewayservice/common/cache/ServiceCache.java
+++ src/main/java/com/cu2mber/gatewayservice/common/cache/ServiceCache.java
@@ -2,6 +2,8 @@ package com.cu2mber.gatewayservice.common.cache;
 
 import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Service;
 
@@ -13,16 +15,19 @@ import java.util.concurrent.TimeUnit;
 public class ServiceCache {
 
     private final DiscoveryClient discoveryClient;
+    private final long ttlSeconds;
     private Cache<String, List<String>> cache;
 
-    public ServiceCache(DiscoveryClient discoveryClient) {
+    public ServiceCache(DiscoveryClient discoveryClient,
+                        @Value("${gateway.cache.ttl-seconds:60}") long ttlSeconds) {
         this.discoveryClient = discoveryClient;
+        this.ttlSeconds = ttlSeconds;
     }
 
     @PostConstruct
     public void init() {
         this.cache = Caffeine.newBuilder()
-                .expireAfterWrite(60, TimeUnit.SECONDS)
+                .expireAfterWrite(ttlSeconds, TimeUnit.SECONDS)
                 .build();
     }
 
